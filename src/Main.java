2eb11f9bd5c7b5a8670fc8c36a4f9e7aff787208
altynbek695 <shakirov.altyn695@gmail.com>
2022-12-03 15:40:37 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
        try( FileReader fileReader= new FileReader("Alfavit.txt")) {
            Scanner scanner= new Scanner(fileReader);
-           int counter= 0;
+           int counter= 1;
            while (scanner.hasNextLine()){
-               counter++;
-               System.out.println(counter+":"+scanner.nextLine());
+
+               System.out.println(counter+++":"+scanner.nextLine());
 
 
            }
@@ -26,44 +26,17 @@ public class Main {
 
     private static void fileAlphabet() {
         try (FileWriter filterWriter= new FileWriter("Alfavit.txt")){
-            filterWriter.write("Aa");
-            filterWriter.write("\nBb");
-            filterWriter.write("\nCc");
-            filterWriter.write("\nDd");
-            filterWriter.write("\nEe");
-            filterWriter.write("\nFf");
-            filterWriter.write("\nGg");
-            filterWriter.write("\nHh");
-            filterWriter.write("\nIi");
-            filterWriter.write("\nJj");
-            filterWriter.write("\nKk");
-            filterWriter.write("\nLl");
-            filterWriter.write("\nMm");
-            filterWriter.write("\nNn");
-            filterWriter.write("\nOo");
-            filterWriter.write("\nPp");
-            filterWriter.write("\nQq");
-            filterWriter.write("\nRr");
-            filterWriter.write("\nSs");
-            filterWriter.write("\nTt");
-            filterWriter.write("\nUu");
-            filterWriter.write("\nVv");
-            filterWriter.write("\nWw");
-            filterWriter.write("\nXx");
-            filterWriter.write("\nYy");
-            filterWriter.write("\nZz");
-            filterWriter.write("\n0");
-            filterWriter.write("\n1");
-            filterWriter.write("\n2");
-            filterWriter.write("\n3");
-            filterWriter.write("\n4");
-            filterWriter.write("\n5");
-            filterWriter.write("\n7");
-            filterWriter.write("\n8");
-            filterWriter.write("\n9");
 
+                for (char c = 'A', i = 'a'; c <= 'Z'; c++, i++) {
+                    filterWriter.write(c + " " + i + "\n");
 
-        } catch (IOException e) {
+                }
+                for (int i = 0; i < 10; i++) {
+                    filterWriter.write(i + " \n");
+
+        }
+
+        }catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
